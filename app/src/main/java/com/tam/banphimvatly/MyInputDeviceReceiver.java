eeac9471454f5be7566ec8026617fc3dd6a49bc9
HyVong007 <tabandroidjava@gmail.com>
2017-07-05 11:35:57 +0700

    Nhận lần 1

---
+++ app/src/main/java/com/tam/banphimvatly/MyInputDeviceReceiver.java
@@ -0,0 +1,20 @@
+package com.tam.banphimvatly;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyInputDeviceReceiver extends BroadcastReceiver
+{
+	public MyInputDeviceReceiver()
+	{
+		super();
+		
+	}
+	
+	@Override
+	public void onReceive(Context p1, Intent p2)
+	{
+		
+	}
+}
